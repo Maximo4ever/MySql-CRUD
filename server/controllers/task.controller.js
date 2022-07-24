4f6249eba41fa82e33bfa89a13240dae3ae477ec
Maximo-4ever <maximilianocm06@gmail.com>
2022-07-24 11:10:41 -0500

    Agrega la funcionalidad de obtener las tareas

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,10 +1,19 @@
 import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
-  res.send("Obteniendo tareas");
+export const getTasks = async (req, res) => {
+  const [result] = await pool.query(
+    "SELECT * FROM task ORDER BY createdAt ASC"
+  );
+  res.json(result);
 };
-export const getTask = (req, res) => {
-  res.send("Obteniendo una tarea");
+export const getTask = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM task WHERE id = ?", [
+    req.params.id,
+  ]);
+  if (result.length === 0)
+    return res.status(404).json({ message: "Task not found" });
+
+  res.json(result[0]);
 };
 export const createTask = async (req, res) => {
   const { title, description } = req.body;
