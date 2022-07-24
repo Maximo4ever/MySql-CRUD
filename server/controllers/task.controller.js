fd62ef2e93ba9e996e9c95d7416ee14b03018bc1
Maximo-4ever <maximilianocm06@gmail.com>
2022-07-23 19:37:29 -0500

    Agrega la funcionalidad de crear tareas

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,11 +1,22 @@
+import { pool } from "../db.js";
+
 export const getTasks = (req, res) => {
   res.send("Obteniendo tareas");
 };
 export const getTask = (req, res) => {
   res.send("Obteniendo una tarea");
 };
-export const createTask = (req, res) => {
-  res.send("Creando tarea");
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO task (title, description) VALUES (?, ?)",
+    [title, description]
+  );
+  res.json({
+    id: result.insertId,
+    title,
+    description,
+  });
 };
 export const updateTask = (req, res) => {
   res.send("Actualizando tarea");
