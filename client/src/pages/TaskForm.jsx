f096d31fbd4fb33af58dcbd57152756f4af101ea
Maximo-4ever <maximilianocm06@gmail.com>
2022-08-15 19:03:19 -0500

    Agrega formulario para crear tareas

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,5 +1,48 @@
+import { Form, Formik } from "formik";
+import { createTaskRequest } from "../api/tasks.api";
+
 function TaskForm() {
-  return <div>TaskForm</div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{ title: "", description: "" }}
+        onSubmit={async (values, actions) => {
+          console.log(values);
+          try {
+            const res = await createTaskRequest(values);
+            console.log(res);
+            actions.resetForm();
+          } catch (error) {
+            console.log(error);
+          }
+        }}
+      >
+        {({ handleChange, handleSubmit, values, isSubmitting }) => (
+          <Form onSubmit={handleSubmit}>
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Write a title"
+              onChange={handleChange}
+              value={values.title}
+            />
+            <label htmlFor="">Description</label>
+            <textarea
+              name="description"
+              rows="10"
+              placeholder="Write a description"
+              onChange={handleChange}
+              value={values.description}
+            ></textarea>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Saving..." : "Save"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default TaskForm;
