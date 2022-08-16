5182faa37d2b84995a41be3d85baee367341816a
Maximo-4ever <maximilianocm06@gmail.com>
2022-08-15 23:33:05 -0500

    Agrega la funcion de eliminar tareas en el Frontend

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,11 +1,22 @@
+import { deleteTaskRequest } from "../api/tasks.api";
+
 function TaskCard({ task }) {
+  const handleDelete = async (id) => {
+    try {
+      const res = await deleteTaskRequest(id);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h3>{task.title}</h3>
       <p>{task.description}</p>
       <span>{task.createdAt}</span>
       <span>{task.done === 1 ? "✔️" : "❌"}</span>
-      <button>Delete</button>
+      <button onClick={() => handleDelete(task.id)}>Delete</button>
       <button>Edit</button>
     </div>
   );
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -13,12 +13,14 @@ function TasksPage() {
     loadTasks();
   }, []);
 
+  function RenderTasks() {
+    if (tasks.length === 0) return <h3>No tasks yet</h3>;
+    return tasks.map((task) => <TaskCard task={task} key={task.id} />);
+  }
   return (
     <div>
       <h1>Tasks</h1>
-      {tasks.map((task) => (
-        <TaskCard task={task} key={task.id} />
-      ))}
+      <RenderTasks />
     </div>
   );
 }
