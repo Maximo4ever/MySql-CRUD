9f4ac4b1be9d54aa3d170de29d74887c05cf3b15
Maximo-4ever <maximilianocm06@gmail.com>
2022-08-15 19:18:49 -0500

    Renderiza la lista de tareas

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { getTasksRequest } from "../api/tasks.api";
+import TaskCard from "../components/TaskCard";
+
 function TasksPage() {
-  return <div>TasksPage</div>;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const loadTasks = async () => {
+      const res = await getTasksRequest();
+      setTasks(res.data);
+    };
+    loadTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      {tasks.map((task) => (
+        <TaskCard task={task} key={task.id} />
+      ))}
+    </div>
+  );
 }
 
 export default TasksPage;
